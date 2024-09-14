d289525fb238aed834fe998f5f5af23d1ed71b3c
Sayan Bhattacharyya <sn1f3rt@outlook.com>
2024-09-15 00:11:59 +0530

    feat(utils): add time converter method

--- xnv/utils.py
+++ xnv/utils.py
@@ -13,3 +13,38 @@ def generate_payment_id() -> str:
 
     """
     return "".join(random.choices(string.hexdigits, k=64)).lower()
+
+
+def calculate_seconds_from_time_string(time_string: str) -> int:
+    """
+    Calculate seconds from time string.
+
+    Parameters
+    ----------
+    time_string : str
+        The time string to convert to seconds.
+
+    Returns
+    -------
+    int
+        The number of seconds in the time string.
+
+    """
+    time = time_string.split(" ")
+
+    seconds = 0
+
+    for t in time:
+        if t.endswith("s"):
+            seconds += int(t[:-1])
+
+        if t.endswith("m"):
+            seconds += int(t[:-1]) * 60
+
+        if t.endswith("h"):
+            seconds += int(t[:-1]) * 3600
+
+        if t.endswith("d"):
+            seconds += int(t[:-1]) * 86400
+
+    return seconds
